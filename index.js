31511f07d0a978b63ab46b52d2d9baea3d0e95cd
hroderik <rodrigorodrigues1@protonmail.com>
2024-02-25 03:10:59 -0300

    update

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-document.write('This is a js test')
+var email = 'contact@rodrigues.com';
+var password = '363';
+
+if (email == 'contact@rodrigues.com' && password == '333') {
+    alert('login successful');
+} else {
+    alert('username or login is incorrect');
+}
